86959a9cffe05321fd0eb3fd54ffa92f49ecdf86
salnguyen <sally.nguyen@oracle.com>
2021-03-04 16:45:25 -0600

    update dns and vars

--- dns.tf
+++ dns.tf
@@ -1,13 +1,16 @@
 #DNS Zone
 resource "oci_dns_zone" "dns_zone" {
+    #Required
     compartment_id = var.oci_compartment_id
     name = var.zone_name
     zone_type = var.zone_type
 }
 
-#DNS Record
+#DNS Records
 resource "oci_dns_rrset" "dns_record" {
+    #Required
     domain = var.rrset_domain
     rtype = var.rrset_type
     zone_name_or_id = oci_dns_zone.test_zone.id
 }
+
--- variables.tf
+++ variables.tf
@@ -1,10 +1,14 @@
-variable "zone_name"{}
+variable "zone_name"{
+    default = "drsite.com"
+}
 
 variable "zone_type" {
     default = "Primary"
 }
-variable "rrset_domain"{}
+variable "rrset_domain"{
+    default = "drsite.com"
+}
 
 variable "rrset_rtype"{
-    default ="A"
+    default ="A" 
 }
